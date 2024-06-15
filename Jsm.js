617c4f6555c5c196c2a896b49c63cfff2e25910c
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-06-15 09:20:46 +0500

    Update and rename Jsm.js to Jsm.md

--- Jsm.js
+++
@@ -1,3 +0,0 @@
-let design = 'https://www.figma.com/design/zCn5bEY42aKDnOt5bpHvvX/JSM-YT---Instagram-Clone-(Copy)?node-id=0-1&t=3UHdh4XE9Br438BC-0'
-
-<img align="right" alt="Coder GIF" height=250 width=350 src="https://cdn.dribbble.com/users/730703/screenshots/6581243/avento.gif" />
---
+++ Jsm.md
@@ -0,0 +1,3 @@
+let design = 'https://www.figma.com/design/zCn5bEY42aKDnOt5bpHvvX/JSM-YT---Instagram-Clone-(Copy)?node-id=0-1&t=3UHdh4XE9Br438BC-0'
+
+<img align="right" alt="Coder GIF" height=250 width=350 src="./img/Snapgram.jpg" />
