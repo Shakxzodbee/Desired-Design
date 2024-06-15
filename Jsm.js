4cd6f8cad08b8cc63646f85a6d196bda831db2cd
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-06-15 09:19:58 +0500

    Update Jsm.js

--- Jsm.js
+++ Jsm.js
@@ -1,3 +1,3 @@
 let design = 'https://www.figma.com/design/zCn5bEY42aKDnOt5bpHvvX/JSM-YT---Instagram-Clone-(Copy)?node-id=0-1&t=3UHdh4XE9Br438BC-0'
 
-<img src="./img/Snapgram.jpg" width="128"/>
+<img align="right" alt="Coder GIF" height=250 width=350 src="https://cdn.dribbble.com/users/730703/screenshots/6581243/avento.gif" />
